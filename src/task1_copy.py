1848cc2ecf978735e429c0e4e0e3df9a8046d083
Magnus Grøndalen <magnus.bg@hotmail.com>
2020-02-10 14:08:59 +0100

    Added non-pandas version for task 1

---
+++ src/task1_copy.py
@@ -0,0 +1,90 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+DATA_FOLDER = "../lab1_data/task1/"
+NFET_FILES = ["ngate1.5v.txt","ngate2v.txt", "ngate3v.txt", 
+              "ngate5v.txt", "ngate10v.txt"]
+PFET_FILES = ["pgate1.5v.txt","pgate2v.txt", "pgate3v.txt",
+             "pgate5v.txt", "pgate10v.txt"]
+
+COLORS = ["r","g","b","y","m"]
+INC = 0.25
+
+def line(x1, x2, y1, y2):    # Two-point formula for a straight line 
+    return lambda x: (y2-y1)*(x-x1)/(x2-x1) + y1      
+
+
+
+
+###### N-channel MOSFET transistor ######
+
+sat_i = int(6/INC) # Index for definite saturation of nfets. v_GS=6V-ish
+
+for j, nfet in enumerate(NFET_FILES):
+    v = []
+    i = []
+    with open(DATA_FOLDER+nfet, "r") as infile:
+        infile.readline()
+        infile.readline()
+        for file_line in infile:
+            data_list = file_line.split()
+            v.append( float( data_list[0].replace(",",".") ) )
+            i.append( float( data_list[1].replace(",",".") ) )
+    v = np.array(v)       
+    i = np.array(i)       
+    v = v[:-4];    i = i[:-4]  
+
+    iv_label = r"$v_{GS}=$"+nfet.replace("ngate","").replace(".txt","")
+    plt.plot(v, i, COLORS[j], label=iv_label)
+
+    v_early = np.linspace(-300, v[sat_i], 300)
+    early_line = line(v[sat_i],v[-8],i[sat_i],i[-8])   # v[-8] is experimentally tested to work fine for early voltage line
+    plt.plot(v_early, early_line(v_early), COLORS[j]+"--", label='_nolegend_' ) 
+
+plt.grid()
+plt.legend()
+plt.ylabel(r"Current $i_{DS}$", size=15)
+plt.xlabel(r"Voltage $v_{DS}$", size=15)
+plt.title(r"$i_{DS}$ vs. $v_{DS}$ Plot Characteristics", size=17)
+plt.savefig("nmos_early.png")
+plt.show()
+
+
+
+
+###### P-channel MOSFET transistor ######
+
+sat_i = int(7.5/INC) # Index for definite saturation of pfets. v_GS=-7.5V-ish
+
+for j, pfet in enumerate(PFET_FILES):
+    v = []
+    i = []
+    with open(DATA_FOLDER+pfet, "r") as infile:
+        infile.readline()
+        infile.readline()
+        for file_line in infile:
+            data_list = file_line.split()
+            v.append( float(data_list[0].replace(",",".")) )
+            i.append( float(data_list[1].replace(",",".")) )
+    v = np.array(v)       
+    i = np.array(i)    
+    v = v[:-4];    i = i[:-4]
+
+    iv_label = r"$v_{GS}=$"+pfet.replace("pgate","").replace(".txt","")
+    plt.plot(v, i, COLORS[j], label=iv_label)
+
+    v_early = np.linspace(v[sat_i], 40, 40)
+    early_line = line(v[-2],v[sat_i], i[-2],i[sat_i])
+    plt.plot(v_early, early_line(v_early), COLORS[j]+"--", label='_nolegend_' ) 
+
+plt.grid()
+plt.legend()
+plt.ylabel(r"Current $i_{DS}$", size=15)
+plt.xlabel(r"Voltage $v_{DS}$", size=15)
+plt.title(r"$i_{DS}$ vs. $v_{DS}$ Plot Characteristics", size=17)
+plt.gca().invert_xaxis()
+plt.gca().invert_yaxis()
+plt.savefig("pmos_early.png")
+plt.show()
